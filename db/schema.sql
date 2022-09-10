3870983c0b4e0a6b6ff4a2ad440700e6897192ef
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-09-10 12:16:44 -0600

    Updated schema primary key id

--- db/schema.sql
+++ db/schema.sql
@@ -20,13 +20,14 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee(
- id  INT NOT NULL ,
+ id  INT NOT NULL PRIMARY KEY ,
  first_name VARCHAR(30) NOT NULL,
  last_name VARCHAR(30) NOT NULL,
  role_id INT,
- manager_id INT NOT NULL,
  FOREIGN KEY (role_id)
  REFERENCES role(id)
+ ON DELETE SET NULL,
+ manager_id INT,
  FOREIGN KEY (manager_id)
  REFERENCES employee(id)
  ON DELETE SET NULL
